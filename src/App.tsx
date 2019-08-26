6be7a6de55ac7d35a48460678dd509cf59d9492a
Mihail_Gah <mihail_gah@epam.com>
2019-08-26 12:11:17 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import Dropzone from 'react-dropzone';
+import ImageGallery from 'react-image-gallery';
+import { ReactImageGalleryItem } from 'react-image-gallery';
+import '../node_modules/react-image-gallery/styles/css/image-gallery.css';
+import './App.scss';
+
+const convertType = (type: string | ArrayBuffer | null) => {
+	if (typeof type === 'string') {
+		return type;
+	} else {
+		return '';
+	}
+};
+
+const App: React.FC<{}> = () => {
+	const [imageSrcs, setImageSrcs] = useState([] as ReactImageGalleryItem[]);
+	
+	const handleOnDrop = <T extends File>(acceptedFiles: T[]) => {
+		if (acceptedFiles && acceptedFiles.length > 0) {
+			for (let i = 0; i < acceptedFiles.length; i++) {
+				const reader = new FileReader();
+				reader.onload = () => {
+					imageSrcs.push({
+						original: convertType(reader.result),
+						thumbnail: convertType(reader.result),
+						originalClass: 'original-image'
+					});
+					setImageSrcs([...imageSrcs]);
+				};
+				reader.readAsDataURL(acceptedFiles[i]);
+			}
+		}
+	};
+	
+	imageSrcs.forEach((item, i) => item.description = `${i+1} из ${imageSrcs.length}`);
+	
+  return (
+    <div className='App'>
+      <Dropzone onDrop={handleOnDrop}>
+				{({ getRootProps, getInputProps }) => (
+					<section>
+						<div className='files-dropzone' {...getRootProps()}>
+							<input {...getInputProps()} />
+							<p className='files-drop'>Перетащите файлы в эту область<br />или<br /><span className='span-blue'>Выберите на компьютере</span></p>
+						</div>
+					</section>
+				)}
+			</Dropzone>
+			{imageSrcs.length > 0 && (
+				<ImageGallery
+					items={imageSrcs}
+					useBrowserFullscreen={false}
+				/>
+			)}
+    </div>
+  );
+}
+
+export default App;
